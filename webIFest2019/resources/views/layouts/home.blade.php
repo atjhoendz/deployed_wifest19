16586777166ba462e64bd1c5331980a975a9283b
atjhoendz <achunarmando@gmail.com>
2019-09-21 21:54:48 +0700

    Add meta-inf

--- webIFest2019/resources/views/layouts/home.blade.php
+++ webIFest2019/resources/views/layouts/home.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="IFest atau Informatics Festival merupakan rangkaian acara IT tahunan
+    yang diselenggarakan oleh Himpunan Mahasiswa Teknik Informatika Universitas Padjadjaran sejak
+    tahun 2015.">
+    <meta name="keywords" content="IFest, Informatics Festival, Unpad, universitas padjadjaran, Teknik Informatika Unpad, Seminar Nasional, Lomba IT, himatif, Lomba, Expo, Startup, Big Data, Industri Revolution, industri 4.0 ">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {!! MaterializeCSS::include_full() !!}
     <title>IFest 2019</title>
@@ -13,13 +17,13 @@
     <header>
         @include('partials.header_home')
     </header>
-    
-    <main> 
-        @yield('main') 
+
+    <main>
+        @yield('main')
     </main>
-    
-    <footer class="page-footer blue darken-4"> 
-        @include('partials.footer') 
+
+    <footer class="page-footer blue darken-4">
+        @include('partials.footer')
     </footer>
     <script src="{{url('js/home.js')}}"></script>
 </body>
